5fc4a26860e31a0685066e7ab3950cb66fa4ee14
Jairo <jairojaldintec112022@gmail.com>
2024-06-18 18:02:34 -0300

    termino la implementacion de servicios y hago cambios en app

--- src/main/java/org/com/App.java
+++ src/main/java/org/com/App.java
@@ -1,11 +1,16 @@
 package org.com;
 
 
+import org.com.almacen.service.DroneService;
+import org.com.almacen.service.DroneServiceImpl;
+
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
 
-        System.out.printf("Almacen Drones :)");
-
+        DroneService droneService = new DroneServiceImpl();
+        Scanner scanner = new Scanner(System.in);
 
         
 
--- src/main/java/org/com/almacen/service/DroneServiceImpl.java
+++ src/main/java/org/com/almacen/service/DroneServiceImpl.java
@@ -1,33 +1,54 @@
 package org.com.almacen.service;
 
+import org.com.almacen.dao.DroneDAO;
+import org.com.almacen.dao.DroneDAOImpl;
 import org.com.almacen.model.Drone;
 
 import java.util.List;
 
 public class DroneServiceImpl implements DroneService {
+
+    private DroneDAO droneDAO;
+
+    public DroneServiceImpl(){
+        this.droneDAO = new DroneDAOImpl();
+    }
+
     @Override
     public void agregarDrone(Drone drone) {
-
+        droneDAO.agregarDrone(drone);
     }
 
     @Override
     public void incrementarStock(String codigo, int cantidad) {
-
+        Drone drone = droneDAO.obtenerDrone(codigo);
+        if(drone != null){
+            drone.setStock(drone.getStock() + cantidad);
+            droneDAO.actualizarDrone(drone);
+        }
     }
 
     @Override
     public void decrementarStock(String codigo, int cantidad) {
-
+        Drone drone = droneDAO.obtenerDrone(codigo);
+        if(drone != null && drone.getStock() >= cantidad){
+            drone.setStock(drone.getStock() - cantidad);
+            droneDAO.actualizarDrone(drone);
+        }
     }
 
     @Override
     public void actualizarPrecio(String codigo, double nuevoPrecio) {
-
+        Drone drone = droneDAO.obtenerDrone(codigo);
+        if(drone != null){
+            drone.setPrecioVenta(nuevoPrecio);
+            droneDAO.actualizarDrone(drone);
+        }
     }
 
     @Override
     public List<Drone> listarDrones() {
-        return List.of();
+        return droneDAO.listarDrones();
     }
 
 }
