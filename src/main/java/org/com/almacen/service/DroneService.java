7b0276bb29310b2aa71da9e73dfeca15d3fd469d
Jairo <jairojaldintec112022@gmail.com>
2024-06-17 21:51:15 -0300

    Realizo cambios en mi interfaz de servicios y sus metodos

--- src/main/java/org/com/almacen/service/DroneService.java
+++ src/main/java/org/com/almacen/service/DroneService.java
@@ -1,4 +1,15 @@
 package org.com.almacen.service;
 
+import org.com.almacen.model.Drone;
+
+import java.util.List;
+
 public interface DroneService {
+
+    void agregarDrone(Drone drone);
+    void incrementarStock(String codigo, int cantidad);
+    void decrementarStock(String codigo, int cantidad);
+    void actualizarPrecio(String codigo, double nuevoPrecio);
+    List<Drone> listarDrones();
+
 }
--- src/main/java/org/com/almacen/service/DroneServiceImpl.java
+++ src/main/java/org/com/almacen/service/DroneServiceImpl.java
@@ -1,4 +1,33 @@
 package org.com.almacen.service;
 
+import org.com.almacen.model.Drone;
+
+import java.util.List;
+
 public class DroneServiceImpl implements DroneService {
+    @Override
+    public void agregarDrone(Drone drone) {
+
+    }
+
+    @Override
+    public void incrementarStock(String codigo, int cantidad) {
+
+    }
+
+    @Override
+    public void decrementarStock(String codigo, int cantidad) {
+
+    }
+
+    @Override
+    public void actualizarPrecio(String codigo, double nuevoPrecio) {
+
+    }
+
+    @Override
+    public List<Drone> listarDrones() {
+        return List.of();
+    }
+
 }
