a10028885142393fca9f5d1d228ede1b8422dcd4
Jairo <jairojaldintec112022@gmail.com>
2024-06-18 18:57:55 -0300

    cambios en mi app

--- src/main/java/org/com/App.java
+++ src/main/java/org/com/App.java
@@ -1,9 +1,11 @@
 package org.com;
 
 
+import org.com.almacen.model.Drone;
 import org.com.almacen.service.DroneService;
 import org.com.almacen.service.DroneServiceImpl;
 
+import java.util.List;
 import java.util.Scanner;
 
 public class App {
@@ -11,6 +13,76 @@ public class App {
 
         DroneService droneService = new DroneServiceImpl();
         Scanner scanner = new Scanner(System.in);
+
+        int opcion = 0;
+
+        while (opcion != 6){
+            System.out.println("1. Agregar nuevo drone");
+            System.out.println("2. Incrementar stock de drones");
+            System.out.println("3. Decrementar stock de drones");
+            System.out.println("4. Actualizar precio de drones");
+            System.out.println("5. Listar todos los drones");
+            System.out.println("6. Salir");
+
+            opcion = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (opcion){
+
+                case 1:
+                    System.out.println("Ingrese codigo:");
+                    String codigo = scanner.nextLine();
+                    System.out.println("Ingrese descripcion:");
+                    String descripcion = scanner.nextLine();
+                    System.out.println("Ingrese precio de Venta:");
+                    double precio = scanner.nextDouble();
+                    System.out.println("Ingrese stock:");
+                    int stock = scanner.nextInt();
+                    droneService.agregarDrone(new Drone(codigo, descripcion, precio, stock));
+                    break;
+
+                case 2:
+                    System.out.println("Ingrese codigo:");
+                    codigo = scanner.nextLine();
+                    System.out.println("Ingrese cantidad a incrementar:");
+                    int cantidadInc = scanner.nextInt();
+                    droneService.incrementarStock(codigo, cantidadInc);
+                    break;
+
+                case 3:
+                    System.out.println("Ingrese codigo:");
+                    codigo = scanner.nextLine();
+                    System.out.println("Ingrese cantidad a decrementar:");
+                    int cantidadDec = scanner.nextInt();
+                    droneService.decrementarStock(codigo,cantidadDec);
+                    break;
+
+                case 4:
+                    System.out.println("Ingrese codigo: ");
+                    codigo = scanner.nextLine();
+                    System.out.println("Ingrese nuevo precio:");
+                    precio = scanner.nextDouble();
+                    droneService.actualizarPrecio(codigo,precio);
+                    break;
+
+                case 5:
+                    List<Drone> drones = droneService.listarDrones();
+
+                    for(Drone drone: drones){
+                        System.out.println(drone);
+                    }
+                    break;
+
+                case 6:
+                    System.out.println("Saliendo...");
+                    break;
+
+                default:
+                    System.out.println("Opcion no valida.");
+            }
+        }
+
+        scanner.close();
 
         
 
